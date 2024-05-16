29ed54f2569f71994f39901efd59a66531ed9c99
PatoProgramador <skpates18@gmail.com>
2024-05-15 21:48:58 -0500

    Search book by name

--- src/main/java/com/patoLearn/desafio/principal/Principal.java
+++ src/main/java/com/patoLearn/desafio/principal/Principal.java
@@ -6,8 +6,11 @@ import com.patoLearn.desafio.service.ConsumoApi;
 import com.patoLearn.desafio.service.ConvierteDatos;
 
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Principal {
+    private Scanner teclado = new Scanner(System.in);
     private static final String URL_BASE = "https://gutendex.com/books/";
     private ConsumoApi consumoApi = new ConsumoApi();
     private ConvierteDatos conversor = new ConvierteDatos();
@@ -25,5 +28,20 @@ public class Principal {
                 .limit(10)
                 .map(l -> l.titulo().toUpperCase())
                 .forEach(System.out::println);
+
+        // Busqueda de libros por nombre
+        System.out.println("Ingrese el nombre del libro que desea buscar: ");
+        String tituloLibro = teclado.nextLine();
+        json = consumoApi.obtenerDatos(URL_BASE + "?search=" + tituloLibro.replace(" ", "+"));
+        Datos datosBusqueda = conversor.obtenerDatos(json, Datos.class);
+        Optional<DatosLibros> libroBuscado = datosBusqueda.resultados().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(tituloLibro.toUpperCase()))
+                .findFirst();
+        if (libroBuscado.isPresent()) {
+            System.out.println("Libro encontrado");
+            System.out.println(libroBuscado.get());
+        } else {
+            System.out.println("Libro no encontrado");
+        }
     }
 }
